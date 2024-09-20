e36f3c713aaad0b8cc944ccca38ac10a7ec7638d
Jhon <jsebas.gomez67@gmail.com>
2024-09-20 12:17:51 -0500

    Endpoint.Database connnection

--- api/config/db_connection.php
+++ api/config/db_connection.php
@@ -15,21 +15,22 @@
     $password = "Unicesmag";
     $dbname = "beta";
     $port = "5432";
-
-       $conn = pg_connect(
-        $host,
-        $username,
-        $password,
-        $dbname,
-        $port
-    );
+    
+    $data_connection = "
+    host=$host
+    port=$port
+    dbname=$dbname
+    user=$username
+    password=$password
+    ";
+    $conn = pg_connect($data_connection);
 
     if(!$conn){
         die("Connection failed: ". pg_last_error());
     }else{
         echo "Connected successfully";
     }
-    
+
     pg_close($conn);
 
 ?>
